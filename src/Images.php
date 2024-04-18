d21b7f66f66a22e42e348374eb31ad32e4710a9c
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-04-18 13:31:58 +0700

    fix

--- src/Images.php
+++ src/Images.php
@@ -1,1 +1,11 @@
 <?php
+namespace Images;
+class Images{
+    public function uploadImage($path, $images, $imagepath): bool|string
+    {
+        if(move_uploaded_file($images['img']['tmp_name'], $path . $imagepath)){
+            return $imagepath;
+        }else{
+            return false;        }
+    }
+}
--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,7 @@
 <?php
+namespace Images;
+
+function upload_picture(string $path = '', array $images = [], string $imagepath = ''): Images
+{
+    return new Images($path, $images, $imagepath);
+}
